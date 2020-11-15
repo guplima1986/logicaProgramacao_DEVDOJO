a6192ea1bee7bca68c1cb1e8f023011dc8f6b28f
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-15 11:02:44 +0000

    Variáveis e tipos primitivos

---
+++ src/Variaveis.java
@@ -0,0 +1,12 @@
+public class Variaveis {
+
+    public static void main(String[] args) {
+
+        int intNumber = 35;
+        double doubleNumber = 3.5;
+
+        System.out.println("Inteiro " + intNumber);
+        System.out.println("Double " + doubleNumber);
+
+    }
+}
