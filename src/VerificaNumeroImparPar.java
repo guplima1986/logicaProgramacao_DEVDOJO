6c131902b435672cac5be53089e02e065f52b04a
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-15 14:16:33 +0000

    Lógica Virado no Jiraya 16 - Controle de fluxo IF - ELSE pt 04

---
+++ src/VerificaNumeroImparPar.java
@@ -0,0 +1,22 @@
+public class VerificaNumeroImparPar {
+
+    public static void main(String[] args) {
+
+        /*
+        * Dado um numero inteiro verifique se o numero é impar ou par
+        * */
+
+        int number = 0;
+
+        if ( number % 2 == 0 ) {
+            System.out.println("Número é PAR");
+        } else {
+            System.out.println("Número é IMPAR");
+        }
+
+
+
+
+
+    }
+}
