00fa6994c7ce9d777b866e6c325830306ba1c700
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-15 12:27:10 +0000

    Lógica Virado no Jiraya 13 - Controle de fluxo if-else pt 01

---
+++ src/ControleDeFluxoIf.java
@@ -0,0 +1,34 @@
+public class ControleDeFluxoIf {
+
+    public static void main(String[] args) {
+
+        System.out.println("Antes do IF");
+
+        if (true) {
+            System.out.println("Se TRUE faz esse trecho");
+        }
+
+        System.out.println("Depois do IF");
+        System.out.println("");
+
+        System.out.println("Antes do IF 2");
+        if (false) {
+            System.out.println("Se TRUE faz esse trecho");
+        }
+        System.out.println("Depois do IF 2");
+
+        System.out.println("");
+
+        System.out.println("Antes do IF 3");
+        boolean condicao = false;
+        System.out.println("A condição é "+condicao);
+        if (condicao) {
+            System.out.println("Dentro IF 3");
+        } else {
+            System.out.println("Dentro ELSE 3");
+        }
+        System.out.println("Depois do IF 3");
+
+
+    }
+}
