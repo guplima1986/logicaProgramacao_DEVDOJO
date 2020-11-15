abde92dd543a69b1e100e6b8cb5a0bf8a3db671b
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-15 12:16:24 +0000

    Lógica Virado no Jiraya 12 - String, char e boolean

---
+++ src/StringCharBoolean.java
@@ -0,0 +1,31 @@
+public class StringCharBoolean {
+
+    public static void main(String[] args) {
+
+        char male = 'M';
+        char female = 'F';
+
+        //Apenas um caractere
+       //char teste = 'FM';
+        System.out.println("CHAR");
+        System.out.println(male);
+        System.out.println(female);
+
+        String masculino = "M";
+        String feminino = "F";
+        String teste2 = "FM";
+
+        System.out.println("STRING");
+        System.out.println(masculino);
+        System.out.println(feminino);
+        System.out.println(teste2);
+
+        boolean condicaoFalse = false;
+        boolean condicaoTrue = true;
+        System.out.println("BOOLEAN");
+        System.out.println(condicaoTrue);
+        System.out.println(condicaoFalse);
+
+
+    }
+}
