f444ee7c7da105ef11a86005677e32b86ccf0525
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-15 11:46:21 +0000

    Lógica Virado no Jiraya 10 - Valores literais + exercícios

---
+++ src/CalculoPorcentagem.java
@@ -0,0 +1,15 @@
+public class CalculoPorcentagem {
+
+    public static void main(String[] args) {
+
+        double valorSalario = 1254D;
+        float valorPorcentagem = 14.5F;
+        double valorFinal;
+
+        valorFinal = (valorSalario * valorPorcentagem)/100;
+
+        System.out.println(valorFinal);
+
+
+    }
+}
