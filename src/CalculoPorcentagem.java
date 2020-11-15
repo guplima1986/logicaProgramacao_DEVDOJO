87493e21c6db4abfb3900c0efeb81b743801dcef
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-15 11:57:49 +0000

    Lógica Virado no Jiraya 11 - Reutilizando variáveis + exercícios

--- src/CalculoPorcentagem.java
+++ src/CalculoPorcentagem.java
@@ -10,6 +10,5 @@ public class CalculoPorcentagem {
 
         System.out.println(valorFinal);
 
-
     }
 }
---
+++ src/PorcentagemSalario.java
@@ -0,0 +1,32 @@
+public class PorcentagemSalario {
+
+
+    public static void main(String[] args) {
+
+        float valorSalario = 5000F;
+        float porcentagem = 30F;
+
+        float porcentagemSalario;
+
+        porcentagemSalario = valorSalario * porcentagem / 100;
+
+        System.out.println("O salário é R$" + valorSalario + " e a porcentagem foi de " + porcentagem);
+        System.out.println("O valor do calculo foi de " + porcentagemSalario);
+
+        porcentagem = 15;
+        porcentagemSalario = valorSalario * porcentagem / 100;
+
+        System.out.println("O salário é R$" + valorSalario + " e a porcentagem foi de " + porcentagem);
+        System.out.println("O valor do calculo foi de " + porcentagemSalario);
+
+        porcentagem = 5;
+        porcentagemSalario = valorSalario * porcentagem / 100;
+
+        System.out.println("O salário é R$" + valorSalario + " e a porcentagem foi de " + porcentagem);
+        System.out.println("O valor do calculo foi de " + porcentagemSalario);
+
+
+
+
+    }
+}
