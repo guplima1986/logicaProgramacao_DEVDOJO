b763c27af74568182b63215b4ce1fc3ed402201e
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-15 11:18:16 +0000

    Lógica Virado no Jiraya 08 - Comentários + Exercícios

---
+++ src/CalculadoraDiferencaIdade.java
@@ -0,0 +1,31 @@
+public class CalculadoraDiferencaIdade {
+
+
+    public static void main(String[] args) {
+
+        minhaSolucao(35, 15);
+        System.out.println("Primeiro teste: "+ minhaSolucao(35, 15));
+        System.out.println("Usando idade negativa: "+ minhaSolucao(35, -15));
+
+        solucaoWillian();
+
+    }
+
+    private static int minhaSolucao(final int primeiraIdade, final int segundaIdade){
+
+        int diferencaIdade = primeiraIdade - segundaIdade;
+        return diferencaIdade;
+    }
+
+    private static void solucaoWillian(){
+
+        int primeiraIdade = 30;
+        int segundaIdade = 15;
+        int diferencaEntreAsIdades = primeiraIdade - segundaIdade;
+
+        System.out.println("Diferença das idades é " + diferencaEntreAsIdades);
+
+
+    }
+
+}
