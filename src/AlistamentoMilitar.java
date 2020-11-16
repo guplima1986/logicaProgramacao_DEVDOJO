2da08c17a1844ea88e8a589150824fdfb9d89cdf
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-16 17:00:37 +0000

    Lógica Virado no Jiraya 24 - Controle de fluxo IF - ELSE pt 11

--- src/AlistamentoMilitar.java
+++ src/AlistamentoMilitar.java
@@ -25,14 +25,12 @@ public class AlistamentoMilitar {
 
         if (sexo.equalsIgnoreCase("M") && idade >= 18) {
             System.out.println("Alistamento militar obrigatorio");
-        } else if (sexo.equalsIgnoreCase("M") && idade < 18) {
+        } else if ((sexo.equalsIgnoreCase("M") || sexo.equalsIgnoreCase("F")) && idade < 18) {
             System.out.println("Alistamento militar não permitido");
         } else if (sexo.equalsIgnoreCase("F") && idade >= 18) {
             System.out.println("Deseja se alistar?");
             System.out.println("S para sim");
             System.out.println("N para não");
-        }  else if (sexo.equalsIgnoreCase("F") && idade < 18) {
-            System.out.println("Alistamento militar não permitido");
         }
 
 
---
+++ src/DiasDaSemana.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class DiasDaSemana {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor correspondente ao dia da semana:");
+        System.out.println("1 = Domingo");
+        System.out.println("2 = Segunda");
+        System.out.println("3 = Terça");
+        System.out.println("4 = Quarta");
+        System.out.println("5 = Quinta");
+        System.out.println("6 = Sexta");
+        System.out.println("7 = Sabado");
+        System.out.println("");
+        int number = scanner.nextInt();
+
+        if (number == 1) {
+            System.out.println("Domingo");
+        } else  if (number == 2) {
+            System.out.println("Segunda-feira");
+        } else  if (number == 3) {
+            System.out.println("Terça-feira");
+        } else  if (number == 4) {
+            System.out.println("Quarta-feira");
+        } else  if (number == 5) {
+            System.out.println("Quinta-feira");
+        } else  if (number == 6) {
+            System.out.println("Sexta-feira");
+        } else  if (number == 7) {
+            System.out.println("Sabado");
+        } else {
+            System.out.println("Número digitado é inválido");
+        }
+
+
+
+
+
+    }
+}
