5f19657cb606b229aafd900d36336a9984403479
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-17 19:26:20 +0000

    Lógica Virado no Jiraya 34 - For pt 02

---
+++ src/aulas/Aula34ForExercicio.java
@@ -0,0 +1,16 @@
+package aulas;
+
+public class Aula34ForExercicio {
+
+    public static void main(String[] args) {
+
+        for ( int i = 15; i <= 200; i++) {
+            System.out.println(i * i);
+        }
+
+        for (int i = 1; i <= 10; i++){
+            System.out.println(5 * i);
+        }
+
+    }
+}
