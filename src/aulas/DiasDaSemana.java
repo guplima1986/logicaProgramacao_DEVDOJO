db2201e5d2a11bad37078e593e0b27a4dfc6cbea
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-17 14:14:25 +0000

    Lógica Virado no Jiraya 26 - Switch case pt 01

--- src/aulas/DiasDaSemana.java
+++ src/aulas/DiasDaSemana.java
@@ -40,6 +40,35 @@ public class DiasDaSemana {
         }
         System.out.println("");
 
+        System.out.println("USANDO SWITCH CASE");
+
+        switch (number){
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Número digitado é inválido");
+                break;
+        }
+
 
 
 
