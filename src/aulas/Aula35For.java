498e8115020e3a32b5e4e42554bb0d48bbc775a4
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-18 16:55:15 +0000

    Lógica Virado no Jiraya 35 - For pt 03

---
+++ src/aulas/Aula35For.java
@@ -0,0 +1,20 @@
+package aulas;
+
+public class Aula35For {
+
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+
+                System.out.println(i + " x " + j + " = " + j * i);
+
+            }
+        }
+
+    }
+
+
+
+}
