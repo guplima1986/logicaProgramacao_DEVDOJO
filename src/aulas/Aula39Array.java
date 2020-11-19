08df20f5d8eb5353229a38dc0246346324506236
Gustavo Lima <gustavo.pereira.lima@gmail.com>
2020-11-19 21:13:15 +0000

    Lógica Virado no Jiraya 39 - Arrays pt 04

--- src/aulas/Aula39Array.java
+++ src/aulas/Aula39Array.java
@@ -11,19 +11,20 @@ public class Aula39Array {
         int[] array2 = new int[3];
         int[] array3 = new int[3];
         Scanner leitor = new Scanner(System.in);
+        String texto = "";
 
         for (int i =0; i< array1.length; i++){
-            System.out.println("Digite o primeiro numero");
+            System.out.println("Vetor 1 posição " + i + ". Digite o primeiro numero");
             array1[i] = leitor.nextInt();
-            System.out.println("Digite o segundo numero");
+            System.out.println("Vetor 2 posição " + i + ". Digite o primeiro numero");
             array2[i] = leitor.nextInt();
             array3[i] = array1[i] * array2[i];
-        }
 
-        for (int j = 0; j<array3.length; j++){
-            System.out.println(array3[j]);
-        }
+            texto = texto + array3[i] + " ";
 
+
+            System.out.println(texto);
+        }
 
     }
 }
